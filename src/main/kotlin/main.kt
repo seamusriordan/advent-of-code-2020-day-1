0a0c4dd6d33b0edfd3154a1c92afcc13df0eb498
Seamus Riordan <seamus@riordan.io>
2020-12-01 10:07:02 -0600

    Rename functions and clean up return types

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,40 +6,42 @@ fun main() {
         inputData.add(it.toInt())
     }
 
-    val pair = find2020Sum(inputData)
+    val pair = find2020SumPair(inputData)
     println(pair.first * pair.second!!)
+
+    val triple = find2020SumTriple(inputData)!!
+    println(triple.first * triple.second * triple.third)
 }
 
-fun find2020Sum(entries: List<Int>): Pair<Int, Int?> {
-    return findPairThatSums(2020, entries)
+fun find2020SumPair(entries: List<Int>): Pair<Int, Int?> {
+    return findPairThatSumsTo(2020, entries)!!
 }
 
-fun findPairThatSums(sum: Int, entries: List<Int>): Pair<Int, Int?> {
-    return entries.mapIndexed { index, entry ->
-        val matchingPair = findPairTo(sum, entry, getRemainingEntriesFrom(entries, index))
+fun findPairThatSumsTo(sum: Int, entries: List<Int>): Pair<Int, Int>? {
+    val pair = entries.mapIndexed { index, entry ->
+        val matchingPair = findMatchingSumPair(sum, entry, getRemainingEntriesFrom(entries, index))
         Pair(entry, matchingPair)
-    }.first { pair ->
+    }.firstOrNull { pair ->
         pair.second != null
-    }
+    } ?: return null
+    return Pair(pair.first, pair.second!!)
 }
 
 private fun getRemainingEntriesFrom(entries: List<Int>, index: Int): List<Int> {
     return entries.subList(index + 1, entries.size)
 }
 
-private fun findPairTo(sum: Int, addend: Int, possiblePairs: List<Int>): Int? {
+private fun findMatchingSumPair(sum: Int, addend: Int, possiblePairs: List<Int>): Int? {
     return possiblePairs.firstOrNull { trial -> addend + trial == sum }
 }
 
-fun find2020Triple(entries: List<Int>): Triple<Int, Int, Int?> {
-    return entries.mapIndexed { index, entry ->
+fun find2020SumTriple(entries: List<Int>): Triple<Int, Int, Int>? {
+    val triple = entries.mapIndexed { index, entry ->
         val remaining = getRemainingEntriesFrom(entries, index)
-        var pair = Pair<Int, Int?>(0, null)
-        try {
-            pair = findPairThatSums(2020 - entry, remaining)
-        } catch (_: NoSuchElementException) { }
-        Triple(entry, pair.first, pair.second)
-    }.first { triple ->
-        triple.third != null
-    }
+        val pair = findPairThatSumsTo(2020 - entry, remaining)
+        Triple(entry, pair?.first, pair?.second)
+    }.firstOrNull { triple ->
+        triple.second != null && triple.third != null
+    } ?: return null
+    return Triple(triple.first, triple.second!!, triple.third!!)
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -4,54 +4,60 @@ import org.junit.jupiter.api.Test
 class MainTest {
     @Test
     fun `find2020Sum with list 1010 1010 returns 1010 1010`() {
-        assertEquals(find2020Sum(listOf(1010, 1010)), Pair(1010, 1010))
+        assertEquals(find2020SumPair(listOf(1010, 1010)), Pair(1010, 1010))
     }
 
     @Test
     fun `find2020Sum with list 1009 1011 returns 1009 1011`() {
-        assertEquals(find2020Sum(listOf(1009, 1011)), Pair(1009, 1011))
+        assertEquals(find2020SumPair(listOf(1009, 1011)), Pair(1009, 1011))
     }
 
     @Test
     fun `find2020Sum with list 0 1009 1011 returns 1009 1011`() {
-        assertEquals(find2020Sum(listOf(0, 1009, 1011)), Pair(1009, 1011))
+        assertEquals(find2020SumPair(listOf(0, 1009, 1011)), Pair(1009, 1011))
     }
 
     @Test
     fun `find2020Sum with list 1009 1011 0 returns 1009 1011`() {
-        assertEquals(find2020Sum(listOf(1009, 1011, 0)), Pair(1009, 1011))
+        assertEquals(find2020SumPair(listOf(1009, 1011, 0)), Pair(1009, 1011))
     }
 
     @Test
     fun `find2020Sum with list 1009 0 1011 returns 1009 1011`() {
-        assertEquals(find2020Sum(listOf(1009, 0, 1011)), Pair(1009, 1011))
+        assertEquals(find2020SumPair(listOf(1009, 0, 1011)), Pair(1009, 1011))
     }
 
     @Test
     fun `find2020Sum with list 1010 1009 1011 returns 1009 1011`() {
-        assertEquals(find2020Sum(listOf(1010, 1009, 1011)), Pair(1009, 1011))
+        assertEquals(find2020SumPair(listOf(1010, 1009, 1011)), Pair(1009, 1011))
     }
 
     @Test
-    fun `example criteria`() {
+    fun `example criteria for pair`() {
         val exampleList = listOf(1721, 979, 366, 299, 675, 1456)
 
-        assertEquals(find2020Sum(exampleList), Pair(1721, 299))
-
+        assertEquals(find2020SumPair(exampleList), Pair(1721, 299))
     }
 
     @Test
     fun `find2020Triple with list 505, 505, 1010 returns 505, 505, 1010`() {
-        assertEquals(find2020Triple(listOf(505, 505, 1010)), Triple(505, 505, 1010))
+        assertEquals(find2020SumTriple(listOf(505, 505, 1010)), Triple(505, 505, 1010))
     }
 
     @Test
     fun `find2020Triple with list 0, 505, 505, 1010 returns 505, 505, 1010`() {
-        assertEquals(find2020Triple(listOf(0, 505, 505, 1010)), Triple(505, 505, 1010))
+        assertEquals(find2020SumTriple(listOf(0, 505, 505, 1010)), Triple(505, 505, 1010))
     }
 
     @Test
     fun `find2020Triple with list 505, 0, 505, 1010 returns 505, 505, 1010`() {
-        assertEquals(find2020Triple(listOf(505, 0, 505, 1010)), Triple(505, 505, 1010))
+        assertEquals(find2020SumTriple(listOf(505, 0, 505, 1010)), Triple(505, 505, 1010))
+    }
+
+    @Test
+    fun `example criteria for triple`() {
+        val exampleList = listOf(1721, 979, 366, 299, 675, 1456)
+
+        assertEquals(find2020SumTriple(exampleList), Triple(979, 366, 675))
     }
 }
