b839e4f5f7bba2decf6010ca40b0871a1ab32e34
Seamus Riordan <seamus@riordan.io>
2020-12-01 06:18:22 -0600

    find2020sum returns Pair from entries

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,5 +3,5 @@ fun main() {
 }
 
 fun find2020Sum(entries: List<Int>): Pair<Int, Int> {
-    return Pair(1010, 1010)
+    return Pair(entries[0], entries[1])
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -6,4 +6,9 @@ class MainTest {
     fun `find2020Sum with list 1010 1010 returns 1010 1010`() {
         assertEquals(find2020Sum(listOf(1010, 1010)), Pair(1010, 1010))
     }
+
+    @Test
+    fun `find2020Sum with list 1009 1011 returns 1009 1011`() {
+        assertEquals(find2020Sum(listOf(1009, 1011)), Pair(1009, 1011))
+    }
 }
